dbb388575c1467c7edf970badd5f57bef74c5bbe
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-03 10:28:30 +0100

    Complete notes app exercise one test driving model class

--- notes-app/notesModel.test.js
+++ notes-app/notesModel.test.js
@@ -1,1 +1,23 @@
+const NotesModel = require('./notesModel');
 
+describe('NotesModel class', () => {
+  it('initialises with an empty array returned by .getNotes()', () => {
+    const model = new NotesModel;
+    expect(model.getNotes()).toEqual([]);
+  });
+
+  it('adds notes and then returns them', () => {
+    const model = new NotesModel;
+    model.addNote('Buy milk');
+    model.addNote('Go to the gym');
+    expect(model.getNotes()).toEqual(['Buy milk', 'Go to the gym']);
+  });
+
+  it('resets the notes list to empty', () => {
+    const model = new NotesModel;
+    model.addNote('Buy milk');
+    model.addNote('Go to the gym');
+    model.reset('');
+    expect(model.getNotes()).toEqual([]);
+  });
+});
