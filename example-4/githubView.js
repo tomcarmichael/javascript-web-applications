6df43ddbfac0e5c994e3e45c6ee2bd8983f115aa
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-04 11:25:26 +0100

    Implement dynamic image path displaying GH repo avatar

--- example-4/githubView.js
+++ example-4/githubView.js
@@ -18,10 +18,13 @@ class GithubView {
 
   // Set the content of the #repo-name element to equal the value of the full_name property from the repo data.
   // Set the content of the #repo-description element to equal the value of the description property from the repo data.
-
+  // tomcarmichael/web-applications
   display(repoData) {
     document.querySelector('#repo-name').append(repoData.full_name);
     document.querySelector('#repo-description').append(repoData.description);
+    // Add a new element <img> on the page, and add JS code so that it displays the image
+    const avatarImage = document.querySelector('#avatar-image');
+    avatarImage.src = repoData.owner.avatar_url;
   }
 }
 
